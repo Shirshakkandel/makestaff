11d9ee1b86e1b74ccbef7d226732ace2b3f610a6
Shirshak Kandel <shirshakkandel@gmail.com>
2024-02-24 16:03:45 +0545

    Homepage problem fixed

--- src/app/(public)/_components/HireTalent.tsx
+++ src/app/(public)/_components/HireTalent.tsx
@@ -8,8 +8,8 @@ export default function HireTalent({}: Props) {
   return (
     <section className=" relative mx-auto aspect-[360/216]  w-full lg:aspect-[1440/865]">
       <Image alt="" src={PeopleCollectionLanding.src} fill />
-      <div className="absolute left-[50%] top-[50%]">
-        <p className="translate-x-[-50%] text-[23px] font-[500] leading-[48px] text-[#E7E7E7] lg:text-[56px]  lg:leading-[48px]">
+      <div className="absolute left-[50%] top-[50%]  mx-auto w-[70%] translate-x-[-50%]  text-center lg:left-[50%]">
+        <p className="text-[23px]  font-[500] text-[#E7E7E7] lg:text-[56px]  lg:leading-[48px]">
           Hire Talent Globally
         </p>
       </div>
--- src/app/(public)/_components/NavbarMobileMenu.tsx
+++ src/app/(public)/_components/NavbarMobileMenu.tsx
@@ -42,7 +42,7 @@ export default function NavbarMobileMenu({}: Props) {
             ref={ref}
             className="absolute inset-x-0 top-0 z-50  origin-top-right  p-2 transition"
           >
-            <div className="divide-y-2 divide-gray-50 rounded-lg bg-secondary shadow-lg ring-1 ring-black ring-opacity-5">
+            <div className="divide-y-2 divide-gray-50 rounded-lg bg-secondary bg-gradient-to-tr from-purple-50 to-purple-100 shadow-lg ring-1 ring-black ring-opacity-5">
               <div className="px-5 pb-6 pt-5">
                 <div className="flex items-center justify-between">
                   <Link href="/" className="inline-flex items-center space-x-2">
